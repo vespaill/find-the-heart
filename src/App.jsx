01297bb3689c28290666ff8e57c8a4338aba6302
vespaill <vicander_99@hotmail.com>
2023-10-07 18:35:09 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Envelope from "./components/envelope/Envelope";
+
+function App() {
+   return (
+      <div className="canvas">
+         <div className="map">
+            <Envelope/>
+         </div>
+      </div>
+   );
+}
+
+export default App;
