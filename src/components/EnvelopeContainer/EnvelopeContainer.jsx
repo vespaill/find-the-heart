b1b2655953839d0bbb36042b1fa2da4a67fa1e74
vespaill <vicander_99@hotmail.com>
2023-10-08 14:25:01 -0400

    Tear down concealment animation on second click

--- src/components/EnvelopeContainer/EnvelopeContainer.jsx
+++ src/components/EnvelopeContainer/EnvelopeContainer.jsx
@@ -6,11 +6,20 @@ import x_mark from '../../assets/x-mark.png';
 const EnvelopeContainer = () => {
 
    const [is_revealed, setIsRevealed] = useState(false);
+   const [is_torn, setIsTorn] = useState(false);
+
+   const onClick = () => {
+      if (is_revealed) {
+         setIsTorn(true);
+      } else {
+         setIsRevealed(true);
+      }
+   }
 
    return <>
-      <div className={`concealment ${is_revealed? "reveal" : ""}`}>
+      <div className={`concealment ${is_revealed? "reveal" : ""} ${is_torn? "tear" : ""}`}>
          <svg viewBox="0 0 150 150"
-            onClick={() => setIsRevealed(true)}>
+            onClick={onClick}>
             <polygon points="0,0 0,150 20,130 50,140 70,125 100,140 150,90 140,80 150,75 140,60 150,50 100,10 75,10 55,0 25,10"/>
          </svg>
          <img id='x-mark' src={x_mark}/>
